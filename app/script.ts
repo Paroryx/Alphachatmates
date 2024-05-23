4434fd36d41b0a586db78d53e2c89586ec7f0013
Paroryx <93038439+Paroryx@users.noreply.github.com>
2024-05-24 01:38:36 +0200

    Update script.ts

--- app/script.ts
+++ app/script.ts
@@ -2,9 +2,7 @@ import { Types } from "ably/promises";
 import * as Ably from "ably/promises";
 
 (async () => {
-
-    console.log("Oh hai! 🖤");
-
+    
     const optionalClientId = "optionalClientId"; // When not provided in authUrl, a default will be used.
     const ably = new Ably.Realtime.Promise({ authUrl: `/api/ably-token-request?clientId=${optionalClientId}` });
     const channel = ably.channels.get("some-channel-name");
@@ -30,6 +28,7 @@ import * as Ably from "ably/promises";
         const messagesContainer = document.getElementById("messages");
         messagesContainer.appendChild(messageElement);
     });
+    channel.publish("welcome-message", { message: "<font style='color:yellow'>a new user joined the chat</font>" });
 
 })();
 
