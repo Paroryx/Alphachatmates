c231a5e404091a8bd8ec9fc95f95d2b2ff04e62a
Paroryx <93038439+Paroryx@users.noreply.github.com>
2024-05-24 02:40:28 +0200

    Update script.ts

--- app/script.ts
+++ app/script.ts
@@ -31,13 +31,10 @@ import * as Ably from "ably/promises";
             messageElement.innerHTML = `<span class="welcome-message">${msg.data}</span>`;
         } else {
             var date = new Date(msg.timestamp);
-            var year = date.getFullYear();
-            var month = ('0' + (date.getMonth() + 1)).slice(-2); // Monat beginnt bei 0, deshalb +1
-            var day = ('0' + date.getDate()).slice(-2); // Füge eine führende Null hinzu, wenn der Tag einstellig ist
             var hours = ('0' + date.getHours()).slice(-2);
             var minutes = ('0' + date.getMinutes()).slice(-2);
             var seconds = ('0' + date.getSeconds()).slice(-2);
-            var formattedDateTime = day + '.' + month + '.' + year + ' ' + hours + ':' + minutes + ':' + seconds;
+            var formattedDateTime = hours + ':' + minutes + ':' + seconds;
 
             messageElement.textContent = formattedDateTime+" "+msg.data;
         }
@@ -45,8 +42,12 @@ import * as Ably from "ably/promises";
         const messagesContainer = document.getElementById("messages");
         messagesContainer.appendChild(messageElement);
     });
-    
-    channel.publish("welcome-message",`${username} joined the chat`);
+    var date = new Date(msg.timestamp);
+    var hours = ('0' + date.getHours()).slice(-2);
+    var minutes = ('0' + date.getMinutes()).slice(-2);
+    var seconds = ('0' + date.getSeconds()).slice(-2);
+    var formattedDateTime = hours + ':' + minutes + ':' + seconds;
+    channel.publish("welcome-message",`${formattedDateTime} - ${username} joined the chat`);
 
 })();
 
