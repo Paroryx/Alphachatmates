cd0c772cd4c7b1dc8c651aca8eaafd977c6cc323
Paroryx <93038439+Paroryx@users.noreply.github.com>
2024-05-24 02:24:23 +0200

    Update script.ts

--- app/script.ts
+++ app/script.ts
@@ -9,7 +9,7 @@ import * as Ably from "ably/promises";
     const messages = document.getElementById("messages");
     const form = document.getElementById("form");
     const input = document.getElementById("input") as HTMLInputElement;
-    const username = document.getElementById("username").textContent;
+    const username = setUsername();
 
     form.addEventListener("submit", (e:SubmitEvent) => {
         e.preventDefault();
